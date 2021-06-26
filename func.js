46315e02c9d5b21171e65d24cf6305babdcd1285
Márcio <marciorvneto@gmail.com>
2021-06-26 12:45:18 -0300

    Converted to tpyescript

--- func.js
+++ func.js
@@ -1,3 +1,6 @@
-const myWonderfulFunction = (x) => 10 * x;
-
+var myWonderfulFunction = function (x) { return 10 * x; };
 console.log(myWonderfulFunction(89749874));
+var myOtherFunction = function (a) {
+    return a.x + a.y;
+};
+console.log(myOtherFunction({ x: 23, y: 45 }));
---
+++ func.ts
@@ -0,0 +1,14 @@
+const myWonderfulFunction = (x) => 10 * x;
+
+console.log(myWonderfulFunction(89749874));
+
+interface Args {
+  x: number;
+  y: number;
+}
+
+const myOtherFunction = (a: Args) => {
+  return a.x + a.y;
+};
+
+console.log(myOtherFunction({ x: 23, y: 45 }));
